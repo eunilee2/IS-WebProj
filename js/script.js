44217cc6c0db333dca7ed484bb6fd36dae3c3326
Eunice <eylee2@andrew.cmu.edu>
2023-04-03 01:15:21 -0400

    index

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-function showDescription(descript){
-    $("#description").html("Description: "+ descript)
-}
 
-function hideDescription(){
-    $("#description").html("")
-}
-
-function validate(){
-    var birthday=$("#birthday").val();
-    var d =new Date(birthday);
-    if (d>= new Date()) {
-        alert("Date of birth is not valid. Date has to be before today.");
-        return false;
-    }
-}
 
 
 
